9b6524c1f437ac7e05658b4cba6ff11d65332dab
WaqasHabibofficial <mianwaqasg714l@gmail.com>
2025-11-13 11:45:41 -0800

    Delete _next directory

--- _next/static/cipAF3W6TAlA5q3mJlDtK/_ssgManifest.js
+++
@@ -1,2 +0,0 @@
-self.__SSG_MANIFEST = new Set(["\u002F", "\u002F404", "\u002Fabout", "\u002Fcontact", "\u002Fprivacy", "\u002Fproject\u002F[slug]", "\u002Fprojects"]);
-self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB()
